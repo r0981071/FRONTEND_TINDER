b1c138b3040cbee3ab0ddcc3880433bc0c0ab149
r0981071 <your-email@example.cor0981071@student.thomasmore.be>
2025-08-10 18:16:45 +0200

    changed error (same code in both navs files)

--- src/components/NavigationComponent.vue
+++ src/components/NavigationComponent.vue
@@ -1,17 +1,21 @@
 <template>
-  <nav style="position:absolute; top:0; right:0; padding:1rem;">
+  <nav style="display:flex; justify-content:center;">
     <ul style="display:flex; list-style:none; gap:20px;">
-      <li v-if="!isLoggedIn" @click="go('login')" style="cursor:pointer;">log in</li>
-      <li v-if="!isLoggedIn" @click="go('signup')" style="cursor:pointer;">sign up</li>
-      <li v-if="isLoggedIn"  @click="go('logout')" style="cursor:pointer;">log out</li>
+      <li v-for="(item, i) in items" :key="i"
+          style="cursor:pointer;"
+          @click="$emit('setActivePage', item)">
+        {{ item }}
+      </li>
     </ul>
   </nav>
 </template>
 
 <script>
 export default {
-  name: "NavigationAuth",
-  props: { isLoggedIn: { type: Boolean, required: true } },
-  methods: { go(page){ this.$emit("setActivePage", page); } }
+  name: "NavigationComponent",
+  data() {
+    return { items: ["swipe", "profile", "messages"] };
+  },
 };
 </script>
+
